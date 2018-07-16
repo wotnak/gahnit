642ee2e6d86dfe95a05a1d1dfb0d0fe6e27244c7
wotnak <adamwotnak@gmail.com>
2018-07-16 13:57:32 +0200

    temporarily hide fields that for now won't be used

--- src/views/device/ActionNew.vue
+++ src/views/device/ActionNew.vue
@@ -10,20 +10,20 @@
         <form @submit.prevent="newConservation">
           <label for="date">Data</label>
           <input id="date" type="date" v-model="conservation.date" required />
-          <label for="employee">Pracownik</label>
-          <input id="employee" type="text" v-model="conservation.employee" placeholder="Pracownik" />
-          <label for="start">Rozpoczęcie pracy</label>
-          <input id="start" type="text" v-model="conservation.start" placeholder="7:30" />
-          <label for="end">Zakończenie pracy</label>
-          <input id="end" type="text" v-model="conservation.end" placeholder="9:00" />
-          <span>
-            <label for="elements">Pozycje</label> <button type="button" @click="addElement">Dodaj kolejną</button>
-          </span>
-          <div v-for="element in conservation.elements" v-bind:key="element.id">
-            <input type="text" v-model="element.name" placeholder="Opis">
-            <input type="number" v-model="element.amount" placeholder="Ilość" min="0">
-            <input type="number" v-model="element.price" placeholder="Cena" min="0">
-          </div>
+          <!-- <label for="employee">Pracownik</label> -->
+          <!-- <input id="employee" type="text" v-model="conservation.employee" placeholder="Pracownik" /> -->
+          <!-- <label for="start">Rozpoczęcie pracy</label> -->
+          <!-- <input id="start" type="text" v-model="conservation.start" placeholder="7:30" /> -->
+          <!-- <label for="end">Zakończenie pracy</label> -->
+          <!-- <input id="end" type="text" v-model="conservation.end" placeholder="9:00" /> -->
+          <!-- <span> -->
+            <!-- <label for="elements">Pozycje</label> <button type="button" @click="addElement">Dodaj kolejną</button> -->
+          <!-- </span> -->
+          <!-- <div v-for="element in conservation.elements" v-bind:key="element.id"> -->
+            <!-- <input type="text" v-model="element.name" placeholder="Opis"> -->
+            <!-- <input type="number" v-model="element.amount" placeholder="Ilość" min="0"> -->
+            <!-- <input type="number" v-model="element.price" placeholder="Cena" min="0"> -->
+          <!-- </div> -->
 
           <button>Zapisz</button>
         </form>
