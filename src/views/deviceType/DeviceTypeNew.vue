b4cc6856f304582390f8ebd95e3970dd052a5854
wotnak <adamwotnak@gmail.com>
2018-07-16 10:11:06 +0200

    implement 'create device type' form

--- src/views/deviceType/DeviceTypeNew.vue
+++ src/views/deviceType/DeviceTypeNew.vue
@@ -1,41 +1,28 @@
 <template>
 <div>
-  <h1>Nowy klient</h1>
-  <h4>Informacje</h4>
-  <form v-on:submit.prevent="create">
-    <input
-      auto-focus
-      v-model="name"
-      placeholder="Nazwa"
-      type="text"
-      value={name}
-      required
-    />
-    <input
-      v-model="nip"
-      placeholder="NIP"
-      type="text"
-      value={nip}
-      required
-    />
-    <h4>Adres</h4>
-    <input v-model="address.country" placeholder="Kraj" type="text" value={address.country} required />
-    <input v-model="address.city" placeholder="Miejscowość" type="text" value={address.city} required />
-    <input v-model="address.street" placeholder="Ulica" type="text" value={address.street} />
-    <input v-model="address.building" placeholder="Budynek" type="text" value={address.building} required />
-    <input v-model="address.postCode" placeholder="Kod pocztowy" type="text" value={address.postCode} required />
-    <input v-model="address.postDepartment" placeholder="Poczta" type="text" value={address.postDepartment} />
+  <h1>Nowy typ urządzeń</h1>
+  <form @submit.prevent="create">
+    <label for="name">Pełna nazwa</label>
+    <input id="name" auto-focus v-model="name" placeholder="Skomplikowany podnośny dźwig" type="text" required />
+    <label for="preferedName">Nazwa zwyczajowa</label>
+    <input id="preferedName" v-model="preferedName" placeholder="Dźwig" type="text" required/>
 
-    <button v-bind:class="classObject" type="submit">Dodaj</button>
+    <label for="conservationEveryNDays">Konserwacja co N dni</label>
+    <input id="conservationEveryNDays" v-model="conservationEveryNDays" type="number" step="1" min="0" required/>
+
+    <label for="udtEveryNDays">UDT co N dni</label>
+    <input id="udtEveryNDays" v-model="udtEveryNDays" type="number" step="1" min="0" required/>
+
+    <button>Dodaj</button>
   </form>
 </div>
 </template>
 
 <script>
   import gql from 'graphql-tag'
-  const CREATE_CUSTOMER = gql `
-    mutation CreateCustomerMutation($name: String!, $nip: String!, $address: AddressCreateInput!) {
-      createCustomer(name: $name, nip: $nip, address: $address) {
+  const CREATE_DEVICE_TYPE = gql `
+    mutation CreateDeviceTypeMutation($name: String!, $preferedName: String, $conservationEveryNDays: Int, $udtEveryNDays: Int) {
+      createDeviceType(name: $name, preferedName: $preferedName, conservationEveryNDays: $conservationEveryNDays, udtEveryNDays: $udtEveryNDays) {
         id
       }
     }
@@ -44,78 +31,29 @@
   export default {
     data: () => ({
       name: '',
-      nip: '',
-      // address
-      address: {
-        country: '',
-        city: '',
-        street: '',
-        building: '',
-        postCode: '',
-        postDepartment: '',
-      }
-
+      preferedName: '',
+      conservationEveryNDays: '',
+      udtEveryNDays: ''
     }),
 
-    // Attribute
     methods: {
       create() {
-        const name = this.name
-        const nip = this.nip
-        const address = this.address
-
-        this.name = ''
-        this.nip = ''
-        this.address = {
-          country: '',
-          city: '',
-          street: '',
-          building: '',
-          postCode: '',
-          postDepartment: '',
-        }
-
-        // Mutation
         this.$apollo.mutate({
-          mutation: CREATE_CUSTOMER,
+          mutation: CREATE_DEVICE_TYPE,
           variables: {
-            name,
-            nip,
-            address
+            name: this.name,
+            preferedName: this.preferedName,
+            conservationEveryNDays: this.conservationEveryNDays,
+            udtEveryNDays: this.udtEveryNDays
           },
         }).then((data) => {
-          this.$router.push({ path: `/customers`})
+          this.$router.push({ name: `DeviceTypeList`})
         }).catch((error) => {
           // Error
           alert(`Error from ${error}`)
           console.error(error)
         })
       },
-    },
-
-    computed: {
-      canPost: function () {
-        return {
-          disabled: !this.text && !this.name
-                    && !this.address.country
-                    && !this.address.city
-                    && !this.address.building
-                    && !this.address.postCode
-        }
-      },
-      classObject: function(){
-        return {
-          dim:  this.text && this.name
-                && this.address.country
-                && this.address.city
-                && this.address.building
-                && this.address.postCode
-        }
-      }
     }
   }
 </script>
-
-<style>
-
-</style>
