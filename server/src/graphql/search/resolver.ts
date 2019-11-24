6b21a62451f8e41d2a3f90208e8199418558fc36
wotnak <adamwotnak@gmail.com>
2019-11-24 15:21:20 +0100

    return complete Device object as search result

--- server/src/graphql/search/resolver.ts
+++ server/src/graphql/search/resolver.ts
@@ -1,12 +1,26 @@
 import { Device } from '../../data/Device'
+import { DeviceType } from '../../data/DeviceType'
+import { Action } from '../../data/Action'
 import { Customer } from '../../data/Customer'
+import { nextTerms } from '../../utils'
 
 export const resolver = {
   // Queries
   Query: {
     search: async (parent, { query }, ctx, info) => {
 
-      const matchingDevices = await Device.find({$text: {$search: query}})
+      const matchingDevices = await Device.find({$text: {$search: query}}).lean()
+      await Promise.all(matchingDevices.map(async device => {
+        const deviceType = await DeviceType.findById(device.type).lean()
+        device.type = deviceType
+        const owner = await Customer.findOne({subiektId: device.owner }).lean()
+        device.owner = owner
+        const actions = await Action.find({device: device.id}).lean()
+        device.actions = actions
+        const terms = nextTerms(actions, deviceType.conservationEveryNDays, deviceType.udtEveryNDays)
+        device.nextUDT = terms.udt
+        device.nextConservation = terms.conservation
+      }))
 
       const matchingCustomers = await Customer.find({$text: {$search: query}})
 
